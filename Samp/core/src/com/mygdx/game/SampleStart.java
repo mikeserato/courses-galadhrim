2f17a17eb7bc928bd04a33edf3cae1a4684c5b3c
Marianne Louise Rivera <marianneluching@gmail.com>
2015-07-03 12:55:51 +0800

    m

--- Samp/core/src/com/mygdx/game/SampleStart.java
+++ Samp/core/src/com/mygdx/game/SampleStart.java
@@ -7,19 +7,24 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class SampleStart extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	Texture startButton;
 	Texture exitButton;
+	//ImageButton startImage;
+	//Stage stage;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("startBackground.jpg");
 		startButton = new Texture("start.jpg");
+		//stage = new Stage();
 		exitButton = new Texture("exit.jpg");
+		
 		
 	}
 
@@ -29,8 +34,9 @@ public class SampleStart extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
-		batch.draw(startButton, 415, 150, 150, 78);
-		batch.draw(exitButton, 415, 50, 150, 78);
+		batch.draw(startButton, 275, 150, 130, 70);
+		batch.draw(exitButton, 275, 50, 130, 70);
 		batch.end();
+
 	}
 }
